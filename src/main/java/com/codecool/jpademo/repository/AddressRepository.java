1a3da1c7058efcc20e4b3faa476924c512e53a0a
mkmDEV <mkmdev18@gmail.com>
2019-06-12 17:16:33 +0200

    [hofix] renamed student table to Student in the query

--- src/main/java/com/codecool/jpademo/repository/AddressRepository.java
+++ src/main/java/com/codecool/jpademo/repository/AddressRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
     @Query("UPDATE Address a SET a.country = 'USA' WHERE a.id IN " +
-            "(SELECT s.address.id FROM student s WHERE s.name LIKE :name)")
+            "(SELECT s.address.id FROM Student s WHERE s.name LIKE :name)")
     @Modifying(clearAutomatically = true)
     int updateAllToUsaByStudentName(@Param("name") String name);
 }
